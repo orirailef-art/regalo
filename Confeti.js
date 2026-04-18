fbd701e2eb9e51dd1eb9a2639ae967fb1216bac4
orirailef-art <orirailef@gmail.com>
2026-04-18 04:58:08 -0300

    Update Confeti.js

--- Confeti.js
+++ Confeti.js
@@ -44,7 +44,7 @@ function crearConfeti() {
 // 3. Dibujar y Mover Confeti
 //
 function animarConfeti() {
-  ctxConfeti.fillStyle = "rgba(143, 208, 252, 1)"; // Fondo Rosa Pastel
+  ctxConfeti.fillStyle = "#8fd0fc"; // Fondo Rosa Pastel
   ctxConfeti.fillRect(0, 0, ancho, alto);
 
   for (let i = 0; i < confetis.length; i++) {
