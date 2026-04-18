7f6b2acea1f29cf809dd5bcc062fc66aede1a295
orirailef-art <orirailef@gmail.com>
2026-04-18 06:22:42 -0300

    Update Sorpresa.js

--- Sorpresa.js
+++ Sorpresa.js
@@ -1,40 +1,53 @@
-// Carta
-const tapas = document.querySelectorAll(".regalo");
-const regalos = document.querySelectorAll(".caja");
+window.addEventListener("DOMContentLoaded", () => {
+  // REGALOS
+  const regalos = document.querySelectorAll(".caja");
 
-const modal1 = document.getElementById("modalCarta1");
-const modal2 = document.getElementById("modalCarta2");
+  const modal1 = document.getElementById("modalCarta1");
+  const modal2 = document.getElementById("modalCarta2");
 
-regalos[0].addEventListener("click", () => {
-  modal1.classList.add("activo");
-});
+  if (regalos[0]) {
+    regalos[0].addEventListener("click", () => {
+      modal1.classList.add("activo");
+    });
+  }
 
-regalos[1].addEventListener("click", () => {
-  modal2.classList.add("activo");
-});
+  if (regalos[1]) {
+    regalos[1].addEventListener("click", () => {
+      modal2.classList.add("activo");
+    });
+  }
 
-modal1.addEventListener("click", () => {
-  modal1.classList.remove("activo");
-});
+  if (modal1) {
+    modal1.addEventListener("click", () => {
+      modal1.classList.remove("activo");
+    });
+  }
 
-modal2.addEventListener("click", () => {
-  modal2.classList.remove("activo");
-});
-// Todo Oscuro + Soplido + Canción
-const overlay = document.querySelector(".overlay");
-const soplido = document.getElementById("soplido");
-const cancion = document.getElementById("cancion");
-const llama = document.querySelector(".llama");
+  if (modal2) {
+    modal2.addEventListener("click", () => {
+      modal2.classList.remove("activo");
+    });
+  }
 
-llama.addEventListener("click", () => {
-  soplido.currentTime = 0;
-  soplido.play();
+  // VELA
+  const overlay = document.querySelector(".overlay");
+  const soplido = document.getElementById("soplido");
+  const cancion = document.getElementById("cancion");
+  const llama = document.querySelector(".llama");
 
-  llama.style.animation = "apagar 0.5s forwards"; // forwards -> Ultimo frame (to)
+  if (llama) {
+    llama.addEventListener("click", () => {
+      soplido.currentTime = 0;
+      soplido.play();
 
-  setTimeout(() => {
-    cancion.currentTime = 0;
-    cancion.play();
-    overlay.classList.add("hidden");
-  }, 1000);
+      llama.style.animation = "apagar 0.5s forwards";
+
+      setTimeout(() => {
+        cancion.currentTime = 0;
+        cancion.play();
+
+        if (overlay) overlay.classList.add("hidden");
+      }, 1000);
+    });
+  }
 });
